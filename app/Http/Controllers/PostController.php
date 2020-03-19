3166b9e158f43d682897752362476e41c5a00afd
Engy Ashraf <egy2962@gmail.com>
2020-03-19 13:58:45 -0700

    Add and modify file show  individual post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -51,6 +51,7 @@ class PostController extends Controller
             'title' => 'required',
             'body' => 'required',
         ]);
+        
         //create post
         $post = new Post;
         $post->title = $request->input('title');
@@ -60,6 +61,66 @@ class PostController extends Controller
 
         return redirect('/posts')->with('success','Post Created');
     }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        return view('posts.show')->with('post', $post);
 
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        
+        //edit post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success','Post Updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success','Post Removed');
+
+    }
     
 }
