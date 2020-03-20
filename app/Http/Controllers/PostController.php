95a99e73650289fbebeb3476bb45672a09a6532a
nohaabdo98 <62175401+nohaabdo98@users.noreply.github.com>
2020-03-20 19:41:30 +0200

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,38 +29,5 @@ class PostController extends Controller
         return view('posts.index')->with('posts',$posts);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('posts.create');
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'title' => 'required',
-            'body' => 'required',
-        ]);
-        
-        //create post
-        $post = new Post;
-        $post->title = $request->input('title');
-        $post->body = $request->input('body');
-        $post->user_id = auth()->user()->id;
-        $post->save();
-
-        return redirect('/posts')->with('success','Post Created');
-    }
-
-    
 }
